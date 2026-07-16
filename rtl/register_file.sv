4f490045914800f347033f59a749673d34ac2c0f
Rayanecode247 <Rayane.hab09@gmail.com>
2026-07-15 20:37:51 -0400

    Update register_file.sv

--- rtl/register_file.sv
+++ rtl/register_file.sv
@@ -14,8 +14,18 @@ module register_file #(
 
     logic [WIDTH-1:0] regs [0:(1<<ADDR_WIDTH)-1];
 
-    assign read_data1 = (read_reg1 == '0) ? '0 : regs[read_reg1];
-    assign read_data2 = (read_reg2 == '0) ? '0 : regs[read_reg2];
+    initial begin
+        for (int i = 0; i < (1<<ADDR_WIDTH); i++) begin
+            regs[i] = '0;
+        end
+    end
+
+    assign read_data1 = (read_reg1 == '0) ? '0 :
+                         (reg_write && (write_reg == read_reg1)) ? write_data :
+                         regs[read_reg1];
+    assign read_data2 = (read_reg2 == '0) ? '0 :
+                         (reg_write && (write_reg == read_reg2)) ? write_data :
+                         regs[read_reg2];
 
     always_ff @(posedge clk) begin
         if (reg_write && (write_reg != '0)) begin
